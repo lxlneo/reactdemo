74bb6e8cf55df6695e5b2397f9636eee752f32cc
lixuelong <lixuelong@aixuedai.com>
2017-04-25 16:32:01 +0800

    add demo01
    添加demo01

---
+++ demo01/src/components/body.js
@@ -0,0 +1,11 @@
+import React ,{Component} from 'react';
+
+export default class Body extends Component{
+    render(){
+        return(
+            <p className="App-intro">
+            This is Body
+        </p>
+    )
+    }
+}
