5fcdbd86aaaed9133b91f1324d24da04ff892582
lixuelong <lixuelong@aixuedai.com>
2017-04-27 18:10:55 +0800

    完善checkbox修改

--- demo03/src/components/body.js
+++ demo03/src/components/body.js
@@ -5,33 +5,53 @@ export default class Body extends Component{
         super(props)
         this.state = {
             inputValue:'input',
-            checkbox:false,
-            radioButton:true,
+            radioButton:'',
             select:'D',
-            textarea:'this is textarea'
+            textarea:'this is textarea',
+            checkboxSet:[]
         }
        this.handleChange = this.handleChange.bind(this); //绑定 this
        this.handleSubmit = this.handleSubmit.bind(this);
     }
+    componentWillMount = () => {
+        this.selectedCheckboxes = {};
+    }
     
     handleChange(event){
+        let target = event.target;
         let name = event.target.name;
-        let value = event.target.value;
-        let temp = {};
-        temp[name] = value;
-        this.setState(temp);
+        let value = target.value;
+        if(target.type == 'checkbox'){  // 处理checkbox的选中
+           if(this.selectedCheckboxes[value]){
+               target.checked = false;
+               delete this.selectedCheckboxes[value];
+           }else{
+               target.checked = true;
+               this.selectedCheckboxes[value]=value;  
+           }
+            this.setState({checkboxSet:Object.keys(this.selectedCheckboxes)})
+        }else {
+            let temp = {};
+            temp[name] = value;
+            this.setState(temp);    
+        }
+        
     }
     
     handleSubmit(event){
+        event.preventDefault();
         console.log('submit');
+        console.log(this.state);
     }
     
     render(){
         return(
           <form className="formDemo" onSubmit={this.handleSubmit} >
               <label>文本框<input type="text" name="inputValue" value={this.state.inputValue}  onChange={this.handleChange} /></label>
-              <label>单选<input type="checkbox" name="checkbox"  onChange={this.handleChange} /></label>
-              <label>复选<input type="radio" name="radioButton"  onChange={this.handleChange} /></label>
+              <label>复选1<input type="checkbox" name="checkbox" value="c1"  onChange={this.handleChange} /></label>
+              <label>复选2<input type="checkbox" name="checkbox" value="c2"  onChange={this.handleChange} /></label>
+              <label>单选1<input type="radio" checked={this.state.radioButton == 'r1'} name="radioButton" value="r1"  onChange={this.handleChange} /></label>
+              <label>单选2<input type="radio" checked={this.state.radioButton == 'r2'} name="radioButton"  value="r2" onChange={this.handleChange} /></label>
               <label>文本框<select name="select" value={this.state.select}  onChange={this.handleChange}>
                   <option value="A">A</option>
                   <option value="B">B</option>
@@ -44,6 +64,7 @@ export default class Body extends Component{
                       
                   </textarea>
               </label>
+              <label><button type="submit">提交</button></label>
           </form>
     )
     }
